7ab1bd2f83fcb94da8aa953438dc040200742f07
Joe <josephagrane@gmail.com>
2024-06-30 06:32:56 +0100

    comment to test model

--- api/localise.py
+++ api/localise.py
@@ -31,7 +31,10 @@ class LocalisationModel:
         return LocalisationModel(gradient, y_int, sigma)
 
     def infer(self, distance: float) -> float:
-        return max(0, np.power(10, np.log10(distance) * self._m + self._c))
+        x = max(0, np.power(10, np.log10(distance) * self._m + self._c))
+        print(f"Inferring that we should be at {np.power(10, x)} reading for a distance of {distance}")
+        return x
+
 
     def likelihood(self, expected_volume, observed_volume) -> float:
         coefficient = 1 / np.sqrt(2 * np.pi * self._sigma**2)
